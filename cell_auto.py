195d5a456c310626b0b465f5245f9532d726aa72
Antoine Parize <antoine.parize@server.com>
2019-01-04 16:28:58 +0100

    ajout pourcentage de progression de calcul

--- cell_auto.py
+++ cell_auto.py
@@ -19,8 +19,6 @@ def first_gen(proportion, shape):
     population = np.zeros(shape)
     population[1:-1, 1:-1] = np.random.binomial(1, proportion, (shape[0]-2, shape[1]-2))  #on insère des 1 dans une matrice plus petite pour ne pas gérer les bords
     #[1:-1] pour s'arrêter à l'avant dernière ligne/colonne
-
-    print('first gen')
     return population
 
 def next_generation(pop, nb_survie, nb_surpopulation, nb_naissance) :
@@ -46,11 +44,8 @@ def final_generations(pop, nb_survie, nb_surpopulation, nb_naissance, NB_GENERAT
     """renvoie la dernière generation des n generations définies au préalable"""
     for i_generation in range(NB_GENERATION):
         pop = next_generation(pop, nb_survie, nb_surpopulation, nb_naissance)
-        if np.mean(pop) == 0.0:
+        if np.mean(pop) == 0:
             break
-        #print(np.mean(pop))
-
-    print('final gen')
     return pop
 
 def scan(m,n, pop):
@@ -59,12 +54,8 @@ def scan(m,n, pop):
 
 def proportion_moyenne(nb_survie, nb_surpopulation, nb_naissance , proportion_initiale, TAILLE_GRILLE):
     """renvoie la proportion moyenne de la population finale après N générations"""
-    #for essais in range(n_sim): #changer pour 100
-    #print(f'sim {essais}')
     pop = first_gen(proportion_initiale, TAILLE_GRILLE)
     final_gen = final_generations(pop, nb_survie, nb_surpopulation, nb_naissance, NB_GENERATION)
-    #total rate = np.mean(final_gen)
-
     return np.mean(final_gen)
 
 if __name__ == "__main__":
@@ -75,18 +66,21 @@ if __name__ == "__main__":
     PROP_INITIALE = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     N_SIM = 100
     NB_GENERATION = 50
+    DECOMPTE_MESURES = 9*8*8*9*100
     stats = np.zeros((len(NB_SURVIES),len(NB_SURPOPULATION), len(NB_NAISSANCE), len(PROP_INITIALE), N_SIM))
 
+    compteur = 0
     for i_surv, nb_survie in enumerate(NB_SURVIES):
         for i_surpop, nb_surpopulation in enumerate(NB_SURPOPULATION):
             for i_naiss, nb_naissance in enumerate(NB_NAISSANCE):
                 for i_prop, proportion_initiale in enumerate(PROP_INITIALE):
                     for i_sim in range(N_SIM):
-                        print(nb_survie, nb_surpopulation, nb_naissance)
+                        #print(nb_survie, nb_surpopulation, nb_naissance)
                         stats[i_surv, i_surpop, i_naiss, i_prop, i_sim] = proportion_moyenne(nb_survie, nb_surpopulation, nb_naissance , proportion_initiale, TAILLE_GRILLE)
+                        compteur += 1
+                        print(f'{compteur/DECOMPTE_MESURES} %')
     print("""
     FIN
     """)
 
 stats.dump("matrice_cell_auto.txt")
-#utiliser sns.distplot pour faire les graphs
