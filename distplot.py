689fd3d6805be6d631183893cb294615a14cd256
Antoine Parize <antoine.parize@server.com>
2019-01-04 12:24:02 +0100

    ajout fichier test de plot sans les histogrammes et avec des labels

--- distplot.py
+++ distplot.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 a = np.load("matrice_cell_auto.txt")
 
 
-sns.distplot(a[0,0,0,0,:], color =(0,0,0.25))
-sns.distplot(a[0,1,0,0,:], color =(0,0,0.5))
-sns.distplot(a[1,0,0,0,:], color =(0,0,0.75))
-sns.distplot(a[1,1,0,0,:], color =(0,0,1))
+sns.distplot(a[0,0,0,0,:], hist = 0, color =(0,0,0.25), label = "beebe")
+sns.distplot(a[0,1,0,0,:],  hist = 0,color =(0,0,0.5), label = "allez")
+sns.distplot(a[1,0,0,0,:], hist = 0, color =(0,0,0.75), label = "bim")
+sns.distplot(a[1,1,0,0,:], hist = 0, color =(0,0,1), label = "message")
 
-plt.legend()
+plt.legend(loc = 0)
 plt.show()
